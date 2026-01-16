e6bf6edc6a5b436c3a686c22267d6c49d0bb1a86
Edson Carlos <edsoncarlos@ymail.com>
2026-01-16 19:11:25 -0300

    FEAT: Create Service

---
+++ src/main/java/com/inovationtech/consumer_kafka/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.inovationtech.consumer_kafka.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Service;
+
+import com.inovationtech.consumer_kafka.record.OrderRecord;
+
+@Service
+public class OrderService {
+  @KafkaListener(topicPartitions = @TopicPartition(topic = "napoleon-order-processed", partitions = { "0" }), containerFactory = "orderKafkaListenerContainerFactory")
+  public void orderListener(OrderRecord order) {
+    System.out.println("Received message consumer 01: " + order.name());
+  }
+}
